7fa41ba40bb2b6da03d7d1dbfe544ec6f2269920
vrslxowe <vrslxowe@gmail.com>
2024-05-16 14:20:02 +0900

    로그인을 시도하면 응답 헤더로 액세스 토큰을 받아야한다.

--- src/main/java/com/koreait/rest_2024_05/base/security/SecurityConfig.java
+++ src/main/java/com/koreait/rest_2024_05/base/security/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
         http
                 .cors().disable() // 타 도메인에서 API 호출 가능
                 .csrf().disable() // CSRF 토큰 끄기
-                .httpBasic().disable() // httpBaic 로그인 방식 끄기
+                .httpBasic().disable() // httpBasic 로그인 방식 끄기
                 .formLogin().disable() // 폼 로그인 방식 끄기
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(STATELESS)
